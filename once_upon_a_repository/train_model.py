c055489f33f239b99f20f1758afefd995ed1e35b
allegroai <>
2020-02-02 15:15:37 +0200

    Add "once upon a repository" blog post code

---
+++ once_upon_a_repository/train_model.py
@@ -0,0 +1,286 @@
+import os
+from pathlib2 import Path
+from operator import add
+import numpy as np
+from PIL import Image
+from argparse import ArgumentParser
+from itertools import chain
+
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+from torchvision.datasets.coco import CocoDetection
+
+from torchvision_references.coco_utils import convert_to_coco_api
+from torchvision_references.coco_eval import CocoEvaluator
+from torchvision_references import utils
+
+from utilities import get_iou_types, draw_debug_images, draw_mask, get_model_instance_segmentation
+from transforms import get_transform
+from engines import create_trainer, create_evaluator
+
+from ignite.engine import Events
+
+from trains import Task
+task = Task.init(project_name='Object Detection with TRAINS, Ignite and TensorBoard',
+                 task_name='Train MaskRCNN with torchvision')
+
+configuration_data = {'num_classes': 91, 'lr': 0.005, 'momentum': 0.9, 'weight_decay': 0.0005, 'image_size': 512,
+                      'mask_predictor_hidden_layer': 256}
+configuration_data = task.connect_configuration(configuration_data)
+
+
+
+
+class CocoMask(CocoDetection):
+    def __init__(self, root, annFile, transform=None, target_transform=None, transforms=None, use_mask=True):
+        super(CocoMask, self).__init__(root, annFile, transforms, target_transform, transform)
+        self.transforms = transforms
+        self.use_mask = use_mask
+    
+    def __getitem__(self, index):
+        
+        # find the next annotated image
+        while True:
+            coco = self.coco
+            img_id = self.ids[index]
+            ann_ids = coco.getAnnIds(imgIds=img_id)
+            target = coco.loadAnns(ann_ids)
+            index += 1
+            if len(ann_ids):
+                break
+        
+        path = coco.loadImgs(img_id)[0]['file_name']
+        img = Image.open(os.path.join(self.root, path)).convert('RGB')
+        
+        # From boxes [x, y, w, h] to [x1, y1, x2, y2]
+        new_target = {"image_id": torch.as_tensor(target[0]['image_id'], dtype=torch.int64),
+                      "area": torch.as_tensor([obj['area'] for obj in target], dtype=torch.float32),
+                      "iscrowd": torch.as_tensor([obj['iscrowd'] for obj in target], dtype=torch.int64),
+                      "boxes": torch.as_tensor([obj['bbox'][:2] + list(map(add, obj['bbox'][:2], obj['bbox'][2:]))
+                                                for obj in target], dtype=torch.float32),
+                      "labels": torch.as_tensor([obj['category_id'] for obj in target], dtype=torch.int64)}
+        if self.use_mask:
+            mask = []
+            for i in range(len(target)):
+                mask.append(coco.annToMask(target[i]))
+            if len(mask) > 1:
+                mask = np.stack(tuple(mask), axis=0)
+            new_target["masks"] = torch.as_tensor(mask, dtype=torch.uint8)
+        
+        if self.transforms is not None:
+            img, new_target = self.transforms(img, new_target)
+        
+        return img, new_target
+
+
+def get_data_loaders(train_ann_file, test_ann_file, batch_size, test_size, image_size, use_mask):
+    # first, crate PyTorch dataset objects, for the train and validation data.
+    dataset = CocoMask(
+        root=Path.joinpath(Path(train_ann_file).parent.parent, train_ann_file.split('_')[1].split('.')[0]),
+        annFile=train_ann_file,
+        transforms=get_transform(train=True, image_size=image_size),
+        use_mask=use_mask)
+    dataset_test = CocoMask(
+        root=Path.joinpath(Path(test_ann_file).parent.parent, test_ann_file.split('_')[1].split('.')[0]),
+        annFile=test_ann_file,
+        transforms=get_transform(train=False, image_size=image_size),
+        use_mask=use_mask)
+    
+    labels_enumeration = dataset.coco.cats
+    
+    indices_val = torch.randperm(len(dataset_test)).tolist()
+    dataset_val = torch.utils.data.Subset(dataset_test, indices_val[:test_size])
+
+    # set train and validation data-loaders
+    train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=6,
+                              collate_fn=utils.collate_fn, pin_memory=True)
+    val_loader = DataLoader(dataset_val, batch_size=batch_size, shuffle=False, num_workers=6,
+                            collate_fn=utils.collate_fn, pin_memory=True)
+    
+    return train_loader, val_loader, labels_enumeration
+
+
+def run(task_args):
+    num_classes = configuration_data.get('num_classes')
+    
+    # Set the training device to GPU if available - if not set it to CPU
+    device = torch.cuda.current_device() if torch.cuda.is_available() else torch.device('cpu')
+    torch.backends.cudnn.benchmark = True if torch.cuda.is_available() else False  # optimization for fixed input size
+    
+    model = get_model_instance_segmentation(num_classes, configuration_data.get('mask_predictor_hidden_layer'))
+    
+    # if there is more than one GPU, parallelize the model
+    if torch.cuda.device_count() > 1:
+        print("{} GPUs were detected - we will use all of them".format(torch.cuda.device_count()))
+        model = torch.nn.DataParallel(model)
+    
+    # copy the model to each device
+    model.to(device)
+    
+    # Define train and test datasets
+    iou_types = get_iou_types(model)
+    use_mask = True if "segm" in iou_types else False
+    train_loader, val_loader, labels_enum = get_data_loaders(task_args.train_dataset_ann_file,
+                                                             task_args.val_dataset_ann_file,
+                                                             task_args.batch_size,
+                                                             task_args.test_size,
+                                                             configuration_data.get('image_size'),
+                                                             use_mask)
+    val_dataset = list(chain.from_iterable(zip(*batch) for batch in iter(val_loader)))
+    coco_api_val_dataset = convert_to_coco_api(val_dataset)
+    
+    if task_args.input_checkpoint:
+        print('Loading model checkpoint from '.format(task_args.input_checkpoint))
+        input_checkpoint = torch.load(task_args.input_checkpoint, map_location=torch.device(device))
+        model.load_state_dict(input_checkpoint['model'])
+    
+    writer = SummaryWriter(log_dir=task_args.log_dir)
+    
+    # define Ignite's train and evaluation engine
+    trainer = create_trainer(model, device)
+    evaluator = create_evaluator(model, device)
+    
+    @trainer.on(Events.STARTED)
+    def on_training_started(engine):
+        # construct an optimizer
+        params = [p for p in model.parameters() if p.requires_grad]
+        engine.state.optimizer = torch.optim.SGD(params,
+                                                 lr=configuration_data.get('lr'),
+                                                 momentum=configuration_data.get('momentum'),
+                                                 weight_decay=configuration_data.get('weight_decay'))
+        engine.state.scheduler = torch.optim.lr_scheduler.StepLR(engine.state.optimizer, step_size=3, gamma=0.1)
+        if task_args.input_checkpoint and task_args.load_optimizer:
+            engine.state.optimizer.load_state_dict(input_checkpoint['optimizer'])
+            engine.state.scheduler.load_state_dict(input_checkpoint['lr_scheduler'])
+    
+    
+    @trainer.on(Events.EPOCH_STARTED)
+    def on_epoch_started(engine):
+        model.train()
+        engine.state.warmup_scheduler = None
+        if engine.state.epoch == 1:
+            warmup_iters = min(task_args.warmup_iterations, len(train_loader) - 1)
+            print('Warm up period was set to {} iterations'.format(warmup_iters))
+            warmup_factor = 1. / warmup_iters
+            engine.state.warmup_scheduler = utils.warmup_lr_scheduler(engine.state.optimizer, warmup_iters, warmup_factor)
+    
+    
+    @trainer.on(Events.ITERATION_COMPLETED)
+    def on_iteration_completed(engine):
+        images, targets, loss_dict_reduced = engine.state.output
+        if engine.state.iteration % task_args.log_interval == 0:
+            loss = sum(loss for loss in loss_dict_reduced.values()).item()
+            print("Epoch: {}, Iteration: {}, Loss: {}".format(engine.state.epoch, engine.state.iteration, loss))
+            for k, v in loss_dict_reduced.items():
+                writer.add_scalar("loss/{}".format(k), v.item(), engine.state.iteration)
+            writer.add_scalar("loss/total_loss", sum(loss for loss in loss_dict_reduced.values()).item(), engine.state.iteration)
+            writer.add_scalar("learning rate/lr", engine.state.optimizer.param_groups[0]['lr'], engine.state.iteration)
+        
+        if engine.state.iteration % task_args.debug_images_interval == 0:
+            for n, debug_image in enumerate(draw_debug_images(images, targets)):
+                writer.add_image("training/image_{}".format(n), debug_image, engine.state.iteration, dataformats='HWC')
+                if 'masks' in targets[n]:
+                    writer.add_image("training/image_{}_mask".format(n),
+                                     draw_mask(targets[n]), engine.state.iteration, dataformats='HW')
+        images = targets = loss_dict_reduced = engine.state.output = None
+    
+    
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def on_epoch_completed(engine):
+        engine.state.scheduler.step()
+        evaluator.run(val_loader)
+        for res_type in evaluator.state.coco_evaluator.iou_types:
+            average_precision_05 = evaluator.state.coco_evaluator.coco_eval[res_type].stats[1]
+            writer.add_scalar("validation-{}/average precision 0_5".format(res_type), average_precision_05,
+                              engine.state.iteration)
+        checkpoint_path = os.path.join(task_args.output_dir, 'model_epoch_{}.pth'.format(engine.state.epoch))
+        print('Saving model checkpoint')
+        checkpoint = {
+            'model': model.state_dict(),
+            'optimizer': engine.state.optimizer.state_dict(),
+            'lr_scheduler': engine.state.scheduler.state_dict(),
+            'epoch': engine.state.epoch,
+            'configuration': configuration_data._to_dict(),
+            'labels_enumeration': labels_enum}
+        utils.save_on_master(checkpoint, checkpoint_path)
+        print('Model checkpoint from epoch {} was saved at {}'.format(engine.state.epoch, checkpoint_path))
+        evaluator.state = checkpoint = None
+    
+    
+    @evaluator.on(Events.STARTED)
+    def on_evaluation_started(engine):
+        model.eval()
+        engine.state.coco_evaluator = CocoEvaluator(coco_api_val_dataset, iou_types)
+    
+    
+    @evaluator.on(Events.ITERATION_COMPLETED)
+    def on_eval_iteration_completed(engine):
+        images, targets, results = engine.state.output
+        if engine.state.iteration % task_args.log_interval == 0:
+            print("Evaluation: Iteration: {}".format(engine.state.iteration))
+        
+        if engine.state.iteration % task_args.debug_images_interval == 0:
+            for n, debug_image in enumerate(draw_debug_images(images, targets, results)):
+                writer.add_image("evaluation/image_{}_{}".format(engine.state.iteration, n),
+                                 debug_image, trainer.state.iteration, dataformats='HWC')
+                if 'masks' in targets[n]:
+                    writer.add_image("evaluation/image_{}_{}_mask".format(engine.state.iteration, n),
+                                     draw_mask(targets[n]), trainer.state.iteration, dataformats='HW')
+                    curr_image_id = int(targets[n]['image_id'])
+                    writer.add_image("evaluation/image_{}_{}_predicted_mask".format(engine.state.iteration, n),
+                                     draw_mask(results[curr_image_id]).squeeze(), trainer.state.iteration, dataformats='HW')
+        images = targets = results = engine.state.output = None
+    
+    
+    @evaluator.on(Events.COMPLETED)
+    def on_evaluation_completed(engine):
+        # gather the stats from all processes
+        engine.state.coco_evaluator.synchronize_between_processes()
+        
+        # accumulate predictions from all images
+        engine.state.coco_evaluator.accumulate()
+        engine.state.coco_evaluator.summarize()
+    
+    
+    trainer.run(train_loader, max_epochs=task_args.epochs)
+    writer.close()
+    
+    
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument('--warmup_iterations', type=int, default=5000,
+                        help='Number of iteration for warmup period (until reaching base learning rate)')
+    parser.add_argument('--batch_size', type=int, default=4,
+                        help='input batch size for training and validation (default: 4)')
+    parser.add_argument('--test_size', type=int, default=2000,
+                        help='number of frames from the test dataset to use for validation (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=10,
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--log_interval', type=int, default=100,
+                        help='how many batches to wait before logging training status')
+    parser.add_argument('--debug_images_interval', type=int, default=500,
+                        help='how many batches to wait before logging debug images')
+    parser.add_argument('--train_dataset_ann_file', type=str,
+                        default='~/bigdata/coco/annotations/instances_train2017.json',
+                        help='annotation file of train dataset')
+    parser.add_argument('--val_dataset_ann_file', type=str,
+                        default='~/bigdata/coco/annotations/instances_val2017.json',
+                        help='annotation file of test dataset')
+    parser.add_argument('--input_checkpoint', type=str, default='',
+                        help='Loading model weights from this checkpoint.')
+    parser.add_argument('--load_optimizer', default=False, type=bool,
+                        help='Use optimizer and lr_scheduler saved in the input checkpoint to resume training')
+    parser.add_argument("--output_dir", type=str, default="/tmp/checkpoints",
+                        help="output directory for saving models checkpoints")
+    parser.add_argument("--log_dir", type=str, default="/tmp/tensorboard_logs",
+                        help="log directory for Tensorboard log output")
+    args = parser.parse_args()
+
+    if not os.path.exists(args.output_dir):
+        utils.mkdir(args.output_dir)
+    if not os.path.exists(args.log_dir):
+        utils.mkdir(args.log_dir)
+
+    run(args)
---
+++ once_upon_a_repository/utilities.py
@@ -0,0 +1,105 @@
+import cv2
+import numpy as np
+import attr
+import json
+
+import torch
+
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+from torchvision.transforms import functional as F
+
+
+def draw_boxes(im, boxes, labels, color=(150, 0, 0)):
+    for box, draw_label in zip(boxes, labels):
+        draw_box = box.astype('int')
+        im = cv2.rectangle(im, tuple(draw_box[:2]), tuple(draw_box[2:]), color, 2)
+        im = cv2.putText(im, str(draw_label), (draw_box[0], max(0, draw_box[1]-5)),
+                         cv2.FONT_HERSHEY_COMPLEX, 0.8, color, 2)
+    return im
+
+
+def draw_debug_images(images, targets, predictions=None, score_thr=0.3):
+    debug_images = []
+    for image, target in zip(images, targets):
+        img = draw_boxes(np.array(F.to_pil_image(image.cpu())),
+                         [box.cpu().numpy() for box in target['boxes']],
+                         [label.item() for label in target['labels']])
+        if predictions:
+            img = draw_boxes(img,
+                             [box.cpu().numpy() for box, score in
+                              zip(predictions[target['image_id'].item()]['boxes'],
+                                  predictions[target['image_id'].item()]['scores']) if score >= score_thr],
+                             [label.item() for label, score in
+                              zip(predictions[target['image_id'].item()]['labels'],
+                                  predictions[target['image_id'].item()]['scores']) if score >= score_thr],
+                             color=(0, 150, 0))
+        debug_images.append(img)
+    return debug_images
+
+
+def draw_mask(target):
+    masks = [channel*label for channel, label in zip(target['masks'].cpu().numpy(), target['labels'].cpu().numpy())]
+    masks_sum = sum(masks)
+    masks_out = masks_sum + 25*(masks_sum > 0)
+    return (masks_out*int(255/masks_out.max())).astype('uint8')
+
+
+def get_model_instance_segmentation(num_classes, hidden_layer):
+    # load an instance segmentation model pre-trained on COCO
+    model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+
+    # get number of input features for the classifier
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    # replace the pre-trained head with a new one
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    # now get the number of input features for the mask classifier
+    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+
+    # and replace the mask predictor with a new one
+    model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask, hidden_layer, num_classes)
+    return model
+
+
+def get_iou_types(model):
+    model_without_ddp = model
+    if isinstance(model, torch.nn.parallel.DistributedDataParallel):
+        model_without_ddp = model.module
+    iou_types = ["bbox"]
+    if isinstance(model_without_ddp, torchvision.models.detection.MaskRCNN):
+        iou_types.append("segm")
+    if isinstance(model_without_ddp, torchvision.models.detection.KeypointRCNN):
+        iou_types.append("keypoints")
+    return iou_types
+
+
+@attr.s(auto_attribs=True)
+class CocoLikeAnnotations():
+    def __attrs_post_init__(self):
+        self.coco_like_json: dict = {'images': [], 'annotations': []}
+        self._ann_id: int = 0
+
+    def update_images(self, file_name, height, width, id):
+        self.coco_like_json['images'].append({'file_name': file_name,
+                                         'height': height, 'width': width,
+                                         'id': id})
+
+    def update_annotations(self, box, label_id, image_id, is_crowd=0):
+        segmentation, bbox, area = self.extract_coco_info(box)
+        self.coco_like_json['annotations'].append({'segmentation': segmentation, 'bbox': bbox, 'area': area,
+                                              'category_id': int(label_id), 'id': self._ann_id, 'iscrowd': is_crowd,
+                                              'image_id': image_id})
+        self._ann_id += 1
+
+    @staticmethod
+    def extract_coco_info(box):
+        segmentation = list(map(int, [box[0], box[1], box[0], box[3], box[2], box[3], box[2], box[1]]))
+        bbox = list(map(int, np.append(box[:2], (box[2:] - box[:2]))))
+        area = int(bbox[2] * bbox[3])
+        return segmentation, bbox, area
+
+    def dump_to_json(self, path_to_json='/tmp/inference_results/inference_results.json'):
+        with open(path_to_json, "w") as write_file:
+            json.dump(self.coco_like_json, write_file)
